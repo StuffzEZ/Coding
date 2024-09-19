6059086205b57a5ef9b2c091b47f2413070c3799
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2024-09-19 19:06:07 +0100

    First Commit
    - Added Home Page
    - Added Login Page
    - Added Signup Page
    - Added HTML Lesson 1
    - Added CSS
    - Added JS

---
+++ assets/js/signup.js
@@ -0,0 +1,76 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const lang = urlParams.get('prog-lang')
+const start = urlParams.get('start')
+
+function paramerterTest(parameter) {
+    if (parameter !== undefined){
+        location.replace("https://stuffzez.github.io/learn/signup")
+    }
+}
+
+function getAccessCode() {
+    if (lang == "option1") {
+        if (start == "option1") {
+            return "1001"
+        } else {
+            if (start == "option2") {
+                return "1002"
+            } else {
+                if (start == "option3") {
+                    return "1003"
+                } else {
+                    if (start !== undefined) {
+                        return "0000"
+                    } else {
+                        return "0000"
+                    }
+                }
+            }
+        }
+    } else {
+        if (lang == "option2") {
+            if (start == "option1") {
+                return "2001"
+            } else {
+                if (start == "option2") {
+                    return "2002"
+                } else {
+                    if (start == "option3") {
+                        return "2003"
+                    } else {
+                        if (start !== undefined) {
+                            return "0000"
+                        } else {
+                            return "0000"
+                        }
+                    }
+                }
+            }
+        } else {
+            if (lang == "option3") {
+                if (start == "option1") {
+                    return "3001"
+                } else {
+                    if (start == "option2") {
+                        return "3002"
+                    } else {
+                        if (start == "option3") {
+                            return "3003"
+                        } else {
+                            if (start !== undefined) {
+                                return "0000"
+                            } else {
+                                return "0000"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("accessCode").innerText = getAccessCode();
+});
